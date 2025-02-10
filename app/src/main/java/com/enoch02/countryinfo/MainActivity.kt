c34d8d2e36c6d14c4e8b8b9dad0a7e4ba92c35d0
Enoch <adesanyaenoch@gmail.com>
2025-02-10 16:29:26 +0100

    Initial commit

---
+++ app/src/main/java/com/enoch02/countryinfo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.enoch02.countryinfo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.enoch02.countryinfo.ui.theme.CountryInfoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CountryInfoTheme {
+
+            }
+        }
+    }
+}
