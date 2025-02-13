e3350ae9255af4c364f788586a662751f5f5cd51
Enoch <adesanyaenoch@gmail.com>
2025-02-13 15:54:36 +0100

    feature: display coat of arms

--- app/src/main/java/com/enoch02/countryinfo/ui/screens/country_detail/components/CountryDetailView.kt
+++ app/src/main/java/com/enoch02/countryinfo/ui/screens/country_detail/components/CountryDetailView.kt
@@ -1,6 +1,10 @@
 package com.enoch02.countryinfo.ui.screens.country_detail.components
 
+import androidx.compose.ui.graphics.Color
 import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,10 +12,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
@@ -34,6 +46,7 @@ fun CountryDetailView(
     val continents = country.continents.joinToString(", ")
     val timeZone = country.timezones?.joinToString(", ") ?: ""
     val population = NumberFormat.getNumberInstance().format(country.population)
+    val pagerState = rememberPagerState { 2 }
 
     if (isLandscape) {
         Row(modifier = Modifier.fillMaxWidth()) {
@@ -44,6 +57,14 @@ fun CountryDetailView(
             )
 
             Spacer(Modifier.width(12.dp))
+
+            AsyncImage(
+                model = country.coatOfArms.png,
+                contentDescription = "${country.name.common} coat of arms",
+                modifier = Modifier
+                    .padding(12.dp)
+                    .height(200.dp)
+            )
 
             Column(
                 modifier = Modifier
@@ -69,10 +90,42 @@ fun CountryDetailView(
         }
     } else {
         Column(modifier = modifier) {
-            FlagView(
-                url = country.flags.png,
-                name = country.name.common,
+            HorizontalPager(
+                state = pagerState,
+                pageContent = { page ->
+                    when (page) {
+                        0 -> {
+                            FlagView(
+                                url = country.flags.png,
+                                name = country.name.common,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+
+                        1 -> {
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                AsyncImage(
+                                    model = country.coatOfArms.png,
+                                    contentDescription = "${country.name.common} coat of arms",
+                                    modifier = Modifier
+                                        .padding(12.dp)
+                                        .height(200.dp)
+                                )
+                            }
+                        }
+                    }
+                },
                 modifier = Modifier.fillMaxWidth()
+            )
+            PagerIndicator(
+                pagerState = pagerState, modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 8.dp)
             )
 
             Spacer(Modifier.height(12.dp))
@@ -113,6 +166,25 @@ fun FlagView(modifier: Modifier = Modifier, url: String, name: String) {
             modifier = Modifier
                 .fillMaxSize()
         )
+    }
+}
+
+@Composable
+fun PagerIndicator(modifier: Modifier = Modifier, pagerState: PagerState) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        repeat(pagerState.pageCount) { iteration ->
+            val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+            Box(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(12.dp)
+            )
+        }
     }
 }
 
